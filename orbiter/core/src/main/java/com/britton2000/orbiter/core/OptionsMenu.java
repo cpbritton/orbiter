6a296c5aa9cd7185d60fdf920d84be9710cf65f6
mwbritton <max@britton2000.com>
2013-01-03 10:54:21 -0500

    image rework

---
+++ orbiter/core/src/main/java/com/britton2000/orbiter/core/OptionsMenu.java
@@ -0,0 +1,18 @@
+package com.britton2000.orbiter.core;
+
+//import static playn.core.PlayN.assets;
+//import static playn.core.PlayN.graphics;
+//import static playn.core.PlayN.log;
+//import static playn.core.PlayN.mouse;
+import playn.core.GroupLayer;
+//import playn.core.Image;
+//import playn.core.ImageLayer;
+//import playn.core.Pointer;
+
+public class OptionsMenu {
+
+	public OptionsMenu(int canvaswidth, int canvasheight, final GroupLayer parentLayer) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
